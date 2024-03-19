286d80cc436662e0c130ccc9d049994ba24d9aeb
Ravi Shankar <138933811+8409251542@users.noreply.github.com>
2024-03-19 12:57:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let srcBtn = document.getElementById("btn");
 let hdnBtn =document.getElementById("hide");
 
-
-hdnBtn.style.display="block";
+let page =1;
 srcBtn.addEventListener('click',getImage);
+let imgContain = document.getElementById("imagecontainer")
 
 async function getImage(){
-    hdnBtn.style.visibility="visible";
     let vlue = document.getElementById("default-search").value;
-    console.log("ravi");
-    let data=await fetch("https://pixabay.com/api/?key=42944467-fc9b19e36a7665abf678eec82&q="+encodeURIComponent(vlue)).then(res=>res.json());
+    let data=await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${vlue}&client_id=mxC1VKPkeltpuIjANSzP9MuNl-KE4BRYe-sAfjZOHUI`).then(res=>res.json());
     console.log(data);
+    for (let i = 0; i <10; i++){
+        let div = document.createElement("div");
+        let url =data.results[i].urls.raw;
+        let image = document.createElement("img");
+        image.className="h-auto max-w-full rounded-lg";
+        image.src=url;
+        div.appendChild(image);
+        imgContain.appendChild(div);
+    }
+    // data.results.forEach(element => {
+    //     let div = document.createElement("div");
+    //     div.innerHTML=`123`
+    //     imgContain.appendChild(div);
+    // });
+    page++;
 }
